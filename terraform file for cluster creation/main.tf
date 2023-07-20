e5f66a8b8702ef88503c943b999b67b3f1b9b11c
Ariel Agranovich <ariel.agra@gmail.com>
2023-07-20 10:05:37 +0300

    modified gitignore. added terraform for aks creation

---
+++ terraform file for cluster creation/main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "aks_resource" {
+  name     = "aks_resource"
+  location = "eastus"
+}
+
+resource "azurerm_kubernetes_cluster" "aks_test" {
+  name                = "aks_test"
+  location            = azurerm_resource_group.aks_resource.location
+  resource_group_name = azurerm_resource_group.aks_resource.name
+  dns_prefix          = "exampleaks1"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 2
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+output "client_certificate" {
+  value = nonsensitive(azurerm_kubernetes_cluster.aks_test.kube_config.0.client_certificate)
+}
+
+output "kube_config" {
+  value = nonsensitive(azurerm_kubernetes_cluster.aks_test.kube_config_raw)
+}
