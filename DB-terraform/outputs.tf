643f3c4f5cb2fb45d861c0cb87eed0eb633b2db3
Ariel Agranovich <ariel.agra@gmail.com>
2023-09-11 11:53:15 +0300

    created new chart stracture for providing helm charts

--- DB-terraform/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "db_pip" {
-  value = azurerm_public_ip.db_pip.ip_address
-}
--- terraform file for cluster creation/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "az_connect_command" {
-  value = "az aks get-credentials --resource-group ${azurerm_resource_group.aks_resource.name} --name ${azurerm_kubernetes_cluster.aks_cluster.name}"
-}
